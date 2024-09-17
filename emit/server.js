222ba75a812d92bd03b68185c294e737b24278bc
success21 <iamsaphal21@gmail.com>
2024-09-17 19:55:51 +0545

    add Auth event

---
+++ emit/server.js
@@ -0,0 +1,16 @@
+// const Emitter = require('events');
+
+// class Auth extends Emitter {
+//   register(username) {
+//     console.log('Registered Successfully');
+//     this.emit('registered', username);
+//   }
+// }
+
+// const auth = new Auth();
+// auth.on('registered', (data) => {
+//     console.log(`sending welcome email to ${data}`);
+//   });
+// auth.register('myname@gmail.com');
+
+
